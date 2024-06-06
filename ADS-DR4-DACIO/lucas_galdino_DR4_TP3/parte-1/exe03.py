091522cf492cd0862f9713023b63c14b142e014e
galdinix <lsgaldino27@gmail.com>
2024-06-06 18:11:50 -0300

    commit

--- ADS-DR4-DACIO/lucas_galdino_DR4_TP3/parte-1/exe03.py
+++ ADS-DR4-DACIO/lucas_galdino_DR4_TP3/parte-1/exe03.py
@@ -1,42 +1,36 @@
 import pandas as pd
+import pathlib
 
 # Carregar o arquivo CSV para um DataFrame
-file_path = 'dadoSujoTP3.csv'
-df = pd.read_csv(file_path)
+CUR_DIR = pathlib.Path(__file__).parent.resolve()
+ARQ_ENT = str(CUR_DIR) + "\\dadoSujoTP3.csv"
+df = pd.read_csv(ARQ_ENT)
 
-# Verificar dados vazios
-dados_vazios = df.isnull().sum()
 
-# Verificar tipos de dados
-tipos_dados = df.dtypes
+#para verificar o tipo dos dados
+#print(df.dtypes)
 
-# Identificar valores não numéricos na coluna 'idade'
-df['idade'] = pd.to_numeric(df['idade'], errors='coerce')
+# verificando se existe dados vazios no df
+#print(df.isnull().sum())
 
-# Verificar e remover linhas onde 'idade' não pode ser convertida para numérico
-df = df.dropna(subset=['idade'])
+#para verificar o tipo dos dados
+#print(df.dtypes)
 
-# Corrigir tipos de dados de data
+# remover nulos
+df = df.dropna()
+#print(df.isnull().sum())
+
+# modificando a formatação das datas
 df['data_venda'] = pd.to_datetime(df['data_venda'], errors='coerce')
 df['data_inscricao'] = pd.to_datetime(df['data_inscricao'], errors='coerce')
 
-# Preencher valores nulos em colunas numéricas com a mediana
-df['quantidade_vendida'].fillna(df['quantidade_vendida'].median(), inplace=True)
-df['preco_unitario'].fillna(df['preco_unitario'].median(), inplace=True)
-df['idade'].fillna(df['idade'].median(), inplace=True)
-
-# Preencher valores nulos em colunas de texto com uma string padrão
-df['telefone'].fillna('Desconhecido', inplace=True)
-
-# Corrigir valores inconsistentes
-df['email'] = df['email'].str.replace('..', '.', regex=False)
-
-# Adicionar colunas geradas
-df['total_venda_calculado'] = df['quantidade_vendida'] * df['preco_unitario']
+# adicionando colunas
+df['total_venda'] = df['quantidade_vendida'] * df['preco_unitario']
 df['mes_venda'] = df['data_venda'].dt.month
 
-# Exportar para um arquivo Excel
-arq_saida = 'dadoSujoTP3_limpo.xlsx'
-df.to_excel(arq_saida, index=False)
+# indo para Excel
+CUR_DIR = pathlib.Path(__file__).parent.resolve()
+ARQ_OUT = str(CUR_DIR) + "\\dado-limpo-TP3.xlsx"
+df.to_excel(ARQ_OUT, index=False)
 
-print(df)
+
